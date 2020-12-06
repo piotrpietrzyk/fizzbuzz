269a8b443e8079f6b433f407b3f51d93017b629b
piotrpietrzyk <piotrek.pietrzyk@gmail.com>
2020-12-06 22:33:34 +0100

    [wip] create fizzbuzz algorithm

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,3 +8,17 @@ def validate(start_value, end_value, prompt):
             print("Enter an integer")
         except AssertionError:
             print(f"Enter an integer between {start_value+1} and {end_value-1}")
+
+
+if __name__ == '__main__':
+    start = validate(0, 10000, "start")
+    end = validate(start, 10001, "end")
+    for i in range(start, end+1):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 5 == 0:
+            print('Buzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        else:
+            print(i)
